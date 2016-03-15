5e4fed3415b06c36a28e52e6cc8e0d08ec781aa6
snoop <babysnoop@riseup.net>
2016-03-15 20:01:19 +0200

    adding objections

---
+++ objections.php
@@ -0,0 +1,395 @@
+<?php
+$PageTitle="Objections - savethekaroo";
+$MetaDescription="Objections to SKA South Africa from various parties";
+include('header.php');
+?>
+
+    
+<h1 class="page-header">Objections
+                    <small>Formal objections have been submitted to the Department of Science and Technology</small>
+                </h1>
+
+        
+<h2>Objections from Hantam Net, an internet provider</h2>
+We at HantamNet
+have always tried to make a positive impact on people around us
+and we have tried to make technology available to those who did not previously have
+the advantage of making use of high quality internet. We have, over the past almost
+16 years, built a wire
+less network f
+ro
+m scratch
+and through the years the
+network has
+grown very much
+¡V
+serving mostly
+the farmers and people in outl
+ying areas.
+We are currently supplying internet from Calvinia
+to Williston (in the town as well)
+and from there towards Carnarvon and Fraserburg; also to Brandvlei and Swarkop and
+the Swartkop community and to Kenhardt. We also supply internet to Middelpos and
+towards Sutherland. ALL farmers in this region as wel
+l as all towns will be affected
+if we need to switch off our network. Currently Brandvlei and Middelpos does not
+have ADSL and thus all businesses and members of the community who are using our
+internet to conduct business will also be affected. Most of
+our customers in these
+communities will not be able to afford the alternative offered.
+We have, in order to supply a high quality service, obtained a Telkom Fibre line at a
+cost of almost R30
+000 per month on a 5 year contract. If we are forced to take d
+own
+towers and stop supplying service to most of our customers we will still be bound in
+this contract.
+We have 8 permanent staff members who will also face loosing their
+jobs if we need to discontinue the service.
+Personally we have bonds on properties
+that needs to be paid monthly; without the
+income of HantamNet we will not be able to pay that.
+As a result of SKA¡¦s activities more than one of our customers have been bullied into
+obtaining VOX telecom¡¦s services. They were told that all our equipment
+would be
+switched off
+¡V
+which is true if the the proposed
+frequencies of between 100mhz and
+25.5Ghz will be upheld. Our equipment work on 2.4ghz and 5.8Ghz
+.
+As a result of Ritinitis Pigmentosa I lost my sight 20 years ago. As a blind person I
+was hit very
+hard and had to try and get my rightful place in life again. I do not have
+anything else to fall back on and would thus have to revert to the Department¡¦s
+promise that they would leave all affected parties in the same or better state they were
+in before
+SKA started.
+I do appreciate that SKA presents great opportunities to the scientific mind, but
+feel strongly that this does not
+warra
+nt the human sacrifices which would result.
+An area void of signals will leave an area void of communication and digita
+l media
+for all people here. Put yourselves in our shoes, wean yourselves from all
+communication means for just a few days
+¡V
+I guarantee that you would not feel
+content. All of us have become over dependant on internet
+and communication
+for survival, if
+that is taken away you will put people 40 years in the past.
+The alternative communication systems, satellite land line phones
+and internet
+restricted to the
+home, are slow and outdated by 2014
+and 2015. The Yaclick
+system was installed in 2015 and conside
+red outdated before it's installation.
+We
+are all used to have a choice of different cell phones and cordless telephones and
+we are used to being mobile while we speak
+-
+we all do other things that require
+mobility while we have telephone conversations wit
+h family and friends. A lack of
+mobility will ¡§steal¡¨ time and productivity.
+The Yaclick solution
+¡V
+via satellite
+-
+works out to be more expensive and customers of HantamNet who has both
+Yaclick and HantamNet claim that HantamNet¡¦s solution is better, fa
+ster and more
+reliable. Yaclick costs R160 for 2Gb data and HantamNet is charging R250 for
+5Gb data; Yaclick topups cost R160 for 2Gb and Hantam charges R30/GB.
+The future of the children of the Northern Cape is being totally ignored. A
+disadvantaged grou
+p is being created and our country will pay a high price in the
+long run.
+Children that grow up here will not be as technologically developed as
+children who grow up in areas where there are no limitations on technology. A
+disadvantaged group will result
+. Historically, there has been enough suffering and
+all unacceptable and unfair policies against SA citizens now and in the future,
+must be avoided.
+We have on several occasions tried to obtain more information on what is planned
+and how it was going to a
+ffect us and our business and we were told not to worry
+just yet. No business can do future planning not knowing what the future might
+bring, information is withheld and that is not fair towards us and the people living
+in this area. We are not labratory
+rats, we are human beings with constitutional
+rights!
+Since the discrepancy between the first map, and the second map which the
+Department of Science and technology presented to the public, is so enormous, it
+shows a lack of co
+-
+ordination from various bod
+ies regarding the planning of the
+project.
+It seems that SKA is grabbing at straws, they have not done enough
+homework and impact studies
+¡V
+they have forgotten about things like the wireless
+signal that will have to be removed and that there are people on
+the other side of
+the SKA sights that will be disconnected from the technology that was previously
+available to them.
+SKA is willing to subsidise and help the people they classify as
+affected parties in the footprint of SKA, but these people are also aff
+ected and
+they are not compensated or considered.
+SKA programs have intimidated and traumatised those most
+affected. This is
+unacceptable.
+Thank you
+Jacques Dodd
+S
+ole
+proprietor
+HantamNet
+
+
+<h2>Objections regarding restrictions on aviation, radio signals and the economic impact</h2>
+
+<h2>General Objections</h2>
+Carnarvon  16 February 2016       
+
+                                                                          
+
+
+For kind attention of Minister Pandor                     
+Department of Science and Technology
+Pretoria
+
+ 
+
+
+Dear Minister,
+
+ 
+
+I would be grateful if you would study the following, acknowledge receipt and respond. Thank you.
+
+ 
+
+OBJECTIONS REGARDING THE AAA AND THE REGULATIONS PROTECTING IT.
+
+ 
+
+It is with great sadness that I direct this email to you, as I now experience what it feels like when one feels let down by your own government. The pain is extending further than myself, and is once again hitting our weaker and poorer members of society hardest. Can we not move on from exploitative measures and cease creating disadvantaged minority groups? This is today a disgrace in the civilised world.
+
+ 
+
+I am particularly keen to make a positive impact on my immediate area, my province and my country. I am personally not interested in impressing or pleasing the international community at the expense of the people and the land closest to my heart. Also, I feel no obligation towards satisfying the "international" hunger for fascinating scientific research projects, despite international contributions and expectations involving the SKA project. My priority is to protect my own people, all the people in the Northern Cape.   
+
+ 
+
+I do appreciate that SKA presents great opportunities to the scientific mind, but feel strongly that this does not warrent the human sacrifices which would result. There are surely locations and opportunities for such endeavours where human sacrifice would not be the result, and if it were outside of South Africa, then so be it. In this case South Africa could be a contributor and participate in the resulting findings.
+
+ 
+
+It is not too late to re-think the way forward and allow the Northern Cape and its people a decent future and an opportunity to prosper. I believe many in South Africa truly regard land as sacred, a treasure and the people's most valuable heritage. The people of the Northern Cape have been caring for and farming this land for up to five and six generations.  Why is the Karoo being given away like this?  We cannot change the inequities of the past, but we can work towards a more promising future for the Northern Cape.
+
+ 
+
+I  do not only object to the Astronomically Advantaged Area destroying civilised life as we know it, in the Northern Cape. I also object to Shell's intentions to do fracking and the Uranium mining projects, all greedily competing for their share of the spoils. The Northern Cape has an economy based on agriculture, and has a population of 1,2 million people. Any inconsiderate and greedy body which intrudes into our midst with self centred and destructive intentions, to profit and score a quick Buck is not to be tolerated.
+
+ 
+
+I also reserve the right to object beyond the 22 February 2016,  with regard to the unfolding SKA drama and the Strategic Environmental Assessment which will be running for a long time still, and any other irregularity related to the project.
+
+ 
+
+Below I list some of my objections relating to the regulations concerning the AAA and the AAA itself:
+
+ 
+
+1.  Documents have been repeatedly requested by the public, proving that the correct procedures were followed before the AAA was declared. These documents have up to now not been provided.
+
+ 
+
+2.  Public participation should have occurred before the AAA was declared but up to now no proof of this has been delivered.
+
+ 
+
+3.  Impact studies before the AAA was declared, justifying the declaration, must be presented. Up to now the public's request to see these documents have not been honoured.
+
+ 
+
+4.  Several impact studies did not occur, ran late, were unsatisfying, or were delayed.
+
+ 
+
+5.  Several promises made by SKA representatives during early meetings were not kept, and eventually the minutes to such meetings were lost, and the public's objections were ignored.
+
+ 
+
+6.  The public's request for an impact study to occur before radio waves were "modified" was not honoured. There was no public participation or an impact study to determine the negative effects of the loss of mobile connectivity. ("Modifications", according to Dr Adrian Tiplady)
+
+ 
+
+7.  The alternative communication systems, satellite land line phones and internet restricted to the  home, are slow and outdated by 2014 and 2015. The Vox system was installed in 2015 and considered outdated before it's installation.  
+
+ 
+
+8.  The lack of transparency by SKA officials, who have continuously denied that the loss of connectivity was due to SKA activities. However, each time cellphone signals were lost, the respective area was declared "radio signal free" and therefore, in their opinion, ideal for radio astronomy.
+
+ 
+
+9.  If SKA is not causing the radio silence and it is not needed to modify the cellphone signals, why regulations regarding cellphone radio frequency silence?
+
+ 
+
+10.  The financially weaker members of society are loosing their only chance of connectivity. And those with alternatives systems, such as Spacestream and Vox, are dissatisfied because mobile connectivity is the superior option.
+
+ 
+
+11. The full SKA program was not disclosed and explained at the onset, so all were kept in the dark regarding the possible or probable outcome. The project was allowed to gradually grow, and gradually reduce human activity and rights.  
+
+ 
+
+12.  There was no opportunity given for reasonable public participation due to this.
+
+ 
+
+13.  A referendum should have been held at the onset before the AAA was declared.
+
+ 
+
+14.  Further, a referendum should be held in 2016, before SKA is allowed to buy land and expand, since this is what the public demands.
+
+ 
+
+15.  Normal citizens are faced with overwhelming details, and technical terms in the AAA document. Many  cannot understand or suspect the full implications of the document.
+
+ 
+
+16.  Normal and trusting citizens cannot begin to suspect the destructive forces and resulting decisions, which will alter their lives in a negative manner.
+
+ 
+
+17.  The future of the children of the Northern Cape is being totally ignored. A disadvantaged group is being created and our country will pay a high price in the long run.
+
+ 
+
+18.  Taking the levels of dissatisfaction and mistrust of most affected parties into account, it is politically incorrect to protect anything connected to SKA.
+
+ 
+
+19.  The map discrepancy issue is disturbing. The manager at the SKA site (Losberg and Meysdam) claimed for years that SKA owned enough land to satisfy all it's future land requirements. This person promised local land owners that no ground would be bought by SKA in the area, encouraging landowners to continue investing in improvements.  Later, in October 2015,  the manager of the SKA land acquisition program claimed that the SKA phase 1 footprint (268 000 ha) would satisfy all SKA future land requirements. Finally the 23 Nov 2015, document with regulations protecting the AAA shows a map with 8 spiralling corridors covering the entire central Karoo area ( 140 000 000 ha ).
+
+ 
+
+20.  The lack of transparency, first pacifying the public and only revealing its intentions bit by bit, caused more mistrust, fear and undermined the public.
+
+ 
+
+21.  Since the discrepancy between the first map, and the second map which the Department of Science and technology presented to the public, is so enormous, it shows a lack of co-ordination from various bodies regarding the planning of the project.
+
+ 
+
+22.  SKA programs have intimidated and traumatised those most affected. This is unacceptable.
+
+ 
+
+23.  The claims about the Karoo region being a isolated desert area made by the SKA website, are totally misleading and incorrect, creating a false impression on the world.
+
+ 
+
+24.  SKA phase 1 will affect 46 000 people from 3 municipal areas directly.
+
+ 
+
+25.  The AAA, allowing for phases 2 and 3, affects all municipal areas excluding one in the Northern Cape.
+
+ 
+
+26.  Many people in the Northern Cape have low levels of literacy, and no specific measures have been taken to include them and allow for their participation. They may vote, but they are not afforded an opportunity to partake in this objection process. I object on behalf of those who cannot object in a written form, and on the behalf of the next generation.
+
+ 
+
+27.  I ask that the South African government publicly and internationally disclose the truth about the negative effects of SKA's expansion, and name the amount of people affected. I ask that the South African government communicate with all SKA member states, inform them of these objections.
+
+ 
+
+28.  I ask that international human rights groups and organisations are informed of this situation and requested to comment.  
+
+ 
+
+29.  An open and fair public participation process, guided by international organisations must take place.
+
+ 
+
+30.  Impact studies must follow and be done by an international body, to ensure more neutrality, before SKA may implement its program.
+
+ 
+
+31.  SKA and AAA legal issues must be considered by SKA international partner states to ensure that these countries are not contravening humanitarian considerations which they adhere to at home. The international community may not turn a blind eye, and exploit South African land and/or citizens. This is an affront, reminiscent of the past.
+
+ 
+
+32.  The current SEA or impact studies conducted by CSIR, have not been completed prior to the land acquisition program. I also reserve the right to object to the SEA's research data in case it takes on a slanted character. I reserve this right up until the final CSIR reports are published and the SKA project is decided on.
+
+ 
+
+33.  The CSIR's impact study is running late, never the less SKA phase 1 activities are aggressively pushing ahead, causing the impact study to appear ineffective.  
+
+ 
+
+34.  No legal or emotional support has been afforded up to now for land owners and workers who are endangered of being relocated. These issues have not even been addressed at all, and this is unacceptable.
+
+ 
+
+35.  The government has not provided incentives for new businesses to develop in the area, or to create job opportunities, and compensate for loss of income within our communities. Many farm workers will loose their work. Due to the financial impact once there are fewer farmers in the communities, many more people in towns will become unemployed.
+
+ 
+
+36.  An SKA representative made the following claim: "You may not enter or reside in this region without permission from the management authority. You may not do most things compatible with civilised life - such as erect lights, fly an aeroplane, drive a truck, construct a road, build a house, or broadcast a radio signal - in this region without permission. Even such farm-like activities as combine harvesting and arc-welding will be severely restricted or prohibited".
+ 
+37.  Additionally, at the October 2015 public meeting more restrictions were presented. Generating power, even wind energy, electric fencing, spark plugs (petrol driven vehicles included) and certain machines are either restricted or prohibited completely. Traffic restrictions and public access would be controlled or prohibited in certain areas.
+
+ 
+
+38.  The unemployment figure will rise. It is currently at 33 to 34 % ,according to a 2011 statistic, stated a Carnarvon municipal authority.
+
+ 
+
+39.  Poor people are negatively impacted and many more will become homeless. In Carnarvon 630 people are on the waiting list for state housing. Construction of further homes is apparently not planned and there are many living in informal settlements as well. According to a municipal worker in Carnarvon there are officially 61 homeless people, but this is a "dark" figure as many are sleeping under the open sky and some houses are extremely overcrowded. It is a crime to spend state money on astronomy projects while there is such need in the area involved.
+
+ 
+
+40.  A disadvantaged group will result. Historically, there has been enough suffering and all unacceptable and unfair policies against SA citizens now and in the future, must be avoided.
+
+ 
+
+41.  International states or companies should not plan or part-plan programs on South African soil where negative impacts on our society, environment or on our heritage will occur.
+
+ 
+
+42.  International programs running in the Karoo will not necessarily profit the Karoo's population. These programs will be to the advantage of foreigners.  SKA has shown us over the last few years exactly how this manifests itself, and such a program could escalate into unmanageable proportions.
+
+ 
+
+43.  Once projects have run their due course, the technology is outdated, the resources exhausted and the international interests fade, a sad, ruined, polluted, scarred and neglected place will remain behind.
+
+ 
+
+44.  Our country's economy is challenged and the predictions are not encouraging. We cannot afford to host SKA. We cannot even afford to educate, uplift, employ, house or provide sanitation to many of our citizens. Surely it is time to take a stand and get our priorities right.
+
+ 
+
+45.  I want to feel proud of South Africa. I want less fortunate citizens to feel proud of South Africa too, and us all to believe in our future! Therefore on behalf of myself and all those who do not have a voice, I feel obliged to raise these objections in the strongest way.
+
+ 
+
+Yours sincerely
+
+ 
+
+Susan Boehm
+
+
+<?php
+include('footer.php');
+?>
